4707d12a583f1da42cc0c1871944a2026dbc79e1
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-08-19 04:46:17 -0400

    Default submit value

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 export default function Login() {
-  return (
-    <form>
+  function handleSubmit(event){
+    console.log('Submitted')
+    event.preventDefault();
+  }
+  return ( 
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
@@ -17,7 +22,7 @@ export default function Login() {
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button" >Login</button>
       </p>
     </form>
   );
