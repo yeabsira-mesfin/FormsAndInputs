337533da95e385c0bfdc8d1293a6035bfbf2f6ea
yeabsira-mesfin <yeabsira.mesfin29@gmail.com>
2024-08-20 06:46:09 -0400

    Using built in validation methods

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from './components/Header.jsx';
-// import Signup from './components/Signup.jsx';
+import Signup from './components/Signup.jsx';
 // import Login from './components/Login.jsx';
-import RefLogin from './components/RefLogin.jsx';
+// import RefLogin from './components/RefLogin.jsx';
 
 function App() {
   return (
@@ -9,8 +9,8 @@ function App() {
       <Header />
       <main>
         {/* <Login /> */}
-        <RefLogin/>
-        {/* <Signup/> */}
+        {/* <RefLogin/> */}
+        <Signup/>
       </main>
     </>
   );
